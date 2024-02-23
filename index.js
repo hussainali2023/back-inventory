5000807b9975b8ff5d845166cae79e0a64c5aa2e
Hussain Ali <iamhussainali2@gmail.com>
2024-02-23 08:30:49 +0530

    added userRoutes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import { express } from "express";
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("../db/db")
+const cors = require("cors");
+
+const app = express()
+
+// middleware 
+app.use(cors())
+app.use(bodyParser.json())
+
+
+// database connection 
+connectDB()
+
